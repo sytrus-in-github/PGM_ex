61cafddc446263e15602b1d7bec48394c95c39f1
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2017-05-28 15:58:59 +0200

    implement e step

--- ex5/e_step.m
+++ ex5/e_step.m
@@ -6,6 +6,13 @@ function [ gamma ] = e_step( histogram, pis, mus, sigmas )
 %   mus (K,D) center of gaussians
 %   sigmas (K,D,D) covariance matrices
 %   gamma (K,N)
-    
+    [x,y,z] = size(histogram);
+    k = size(pis,1);
+    densities = zeros(k,x,y,z);
+    for i=1:k
+        densities(i,:,:,:) = pis(i) * gaussian_density(mus(i,:), sigmas(i,:,:), [x,y,z]);
+    end
+    gamma = reshape(densities, k, []);
+    gamma = gamma ./ sum(gamma, 1);
 end
 

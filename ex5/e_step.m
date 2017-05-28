daf451ae8f36ef5b302c4f23dcdbd0ad1dbfeebf
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2017-05-28 17:08:40 +0200

    change interface of functions

--- ex5/e_step.m
+++ ex5/e_step.m
@@ -1,12 +1,11 @@
-function [ gamma ] = e_step( histogram, pis, mus, sigmas )
+function [ gamma ] = e_step( pis, mus, sigmas )
 %E_STEP e-step of EM algorithm for mixture of K guassians on 3D histogram
 %of size X,Y,Z. N = X*Y*Z, D=3
-%   histogram (X,Y,Z)
 %   pis (K) mixture coefficients
 %   mus (K,D) center of gaussians
 %   sigmas (K,D,D) covariance matrices
 %   gamma (K,N)
-    [x,y,z] = size(histogram);
+    x=256; y=x; z=x;
     k = size(pis,1);
     densities = zeros(k,x,y,z);
     for i=1:k
--- ex5/m_step.m
+++ ex5/m_step.m
@@ -1,11 +1,16 @@
-function [ pis_new, mus_new, sigmas_new ] = m_step( histogram, gamma, pis_old, mus_old, sigmas_old )
+function [ pis, mus, sigmas ] = m_step( X, gamma )
 %M_STEP e-step of EM algorithm for mixture of K guassians on 3D histogram
 %of size X,Y,Z. N = X*Y*Z, D=3
-%   histogram (X,Y,Z)
+%   X (N,D) pixel RGB intensities
 %   pis (K) mixture coefficients
 %   mus (K,D) center of gaussians
 %   sigmas (K,D,D) covariance matrices
 %   gamma (K,N)
-
+    n = size(X,1);
+    k = size(pis,1);
+    Nk = sum(gamma, 2);
+    pis = Nk / n;
+    mus = gamma * X ./ Nk;
+    sigmas = ;
 end
 

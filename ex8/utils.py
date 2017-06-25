62a9eefb5dfa5e870e66cc114c464a12851465d4
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2017-06-25 17:19:29 +0200

    add graph cut function

--- ex8/utils.py
+++ ex8/utils.py
@@ -6,6 +6,7 @@ Created on Sun Jun 25 16:21:46 2017
 """
 
 import numpy as np
+import maxflow
 from PIL import Image
 from matplotlib import pyplot as plt
 
@@ -13,6 +14,17 @@ def readImageAsGray(imageName):
     img = Image.open(imageName).convert('L')
     return np.asarray(img, dtype=np.uint8)
     
+def gridGraphCut(unary, binary):
+    """graph cut for 2d grid with pott-model binary cost"""
+    g = maxflow.Graph[float]()
+    nodeids = g.add_grid_nodes(unary.shape[:-1])
+    g.add_grid_edges(nodeids, binary)
+    g.add_grid_tedges(nodeids, unary[:,:,0], unary[:,:,1])
+    cutValue = g.maxflow()
+    isSource = g.get_grid_segments(nodeids)
+
+    return cutValue, isSource
+
 if __name__=="__main__":
     img = readImageAsGray('branchAndMinCut/garden.png')
     print img.shape

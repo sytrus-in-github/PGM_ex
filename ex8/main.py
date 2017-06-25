de393ab31dee426b1af57b8855b00d50ca63fc5d
Roman Kaskman <roman.kaskman@gmail.com>
2017-06-25 18:14:32 +0200

    Fix visualization

--- ex8/main.py
+++ ex8/main.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         node = priority_queue.get()
         domain = node[1]
         if domain[0] == domain[1] - 1 and domain[2] == domain[3] - 1:
-            print domain
+
             _, isSource = get_energy(domain, img_mean, img)
             break
 
@@ -78,6 +78,12 @@ if __name__ == '__main__':
         priority_queue.put((energy1, domain1))
         priority_queue.put((energy2, domain2))
 
-    result_img = (255 * isSource).astype(np.uint8)
-    plt.imshow(result_img, cmap='gray')
+    # result_img = (255 * isSource).astype(np.uint8)
+
+    fg_color = np.mean(isSource * img)
+    bg_color = np.mean((1 - isSource) * img)
+
+    result_img = (fg_color * isSource + bg_color * (1 - isSource)).astype(np.uint8)
+
+    plt.imshow(result_img, cmap='gray', vmin=0, vmax=255)
     plt.show()

97c2a0b99cfcb0520111e819158fece890644cdb
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2017-07-09 17:55:48 +0200

    add file to read text unary data

---
+++ ex10/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+def read_unary(filename):
+    with open(filename, 'r') as filecontent:
+        nbcol, nbrow, nbclass = [int(s) for s in filecontent.readline().split()]
+        data = np.array([float(s) for s in filecontent.readline().split()]).reshape((nbcol, nbrow, nbclass))
+    return data
+    
+if __name__ == '__main__':
+    data = read_unary('outfile.txt')
+    print data.shape
+    print np.max(data), np.min(data), np.mean(data)*21

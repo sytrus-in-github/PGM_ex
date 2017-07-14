42eca9d8ffda7469c1b30d6558131716cb5bd6f3
Roman Kaskman <roman.kaskman@gmail.com>
2017-07-14 15:57:21 +0200

    Binary

--- ex10/utils.py
+++ ex10/utils.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+w1 = 10
+w2 = 3
+theta_alpha = 80
+theta_beta = 13
+theta_gamma = 3
+
 
 def map_energy(unary):
     if unary == 0:
@@ -13,10 +19,14 @@ def read_unary(filename):
         data_list = [float(s) for s in filecontent.readline().split()]
 
         energies = np.array(map(lambda unary: map_energy(unary), data_list)).reshape((nbcol, nbrow, nbclass))
-        
+
         data = np.array(data_list).reshape((nbcol, nbrow, nbclass))
     return data, energies
 
+
+def compute_binary(label1, label2, point1, point2, image):
+    if label1 == label2:
+        return 0
 
 if __name__ == '__main__':
     data, energies = read_unary('outfile.txt')

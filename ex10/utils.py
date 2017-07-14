9a51e5890e8780a67e891365bd273368fa0bd547
Roman Kaskman <roman.kaskman@gmail.com>
2017-07-14 16:37:42 +0200

    Binaries precomputing

--- ex10/utils.py
+++ ex10/utils.py
@@ -14,7 +14,7 @@ def map_energy(unary):
 
 
 def squared_norm(p1, p2):
-    return np.sum((np.array(p1)-np.array(p2))**2)
+    return np.sum((np.array(p1) - np.array(p2)) ** 2)
 
 
 def read_unary(filename):
@@ -28,14 +28,28 @@ def read_unary(filename):
     return data, energies
 
 
-def compute_binary(label1, label2, point1, point2, image):
-    if label1 == label2:
-        return 0.
-
+def compute_binary(point1, point2, image):
     points_square_norm = squared_norm(point1, point2)
     return w1 * np.exp(-points_square_norm / (2 * (theta_alpha ** 2)) -
                        squared_norm(image[point1], image[point2]) / (2 * (theta_beta ** 2))) + \
            w2 * np.exp(-points_square_norm / (2 * (theta_gamma ** 2)))
+
+
+def precompute_binary_map(image, image_name):
+    filename = 'in2329-supplementary_material_10/' + image_name + '.memmap'
+
+    row, col = image.shape
+    binary_map = np.memmap(filename, dtype=np.float32, mode='w+', shape=(row, col, row, col))
+
+    for r in xrange(row):
+        for c in xrange(col):
+
+            for current_r in xrange(row):
+                for current_c in xrange(col):
+                    
+
+
+
 
 if __name__ == '__main__':
     data, energies = read_unary('outfile.txt')
